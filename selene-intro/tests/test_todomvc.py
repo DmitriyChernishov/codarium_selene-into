f2f34247828ac07ab64fb70d91224016c5786e88
DimaQA <dimon.chernishov@gmail.com>
2023-02-17 14:01:35 +0200

    Deleted a variable, more consistent, added a new comments

--- selene-intro/tests/test_todomvc.py
+++ selene-intro/tests/test_todomvc.py
@@ -6,21 +6,17 @@ def test_complete_todo():
     # open TodoMVC page
     browser.open('https://todomvc.com/examples/emberjs/')
 
-    # add todos: 'a', 'b', 'c'
-    # todos should be 'a', 'b', 'c'
+    # Try to add elements in to do
     browser.element(by.id('new-todo')).type('a').press_enter()
-    browser.element(by.id('new-todo')).send_keys('b').press_enter()
+    browser.element(by.id('new-todo')).type('b').press_enter()
     browser.element(by.id('new-todo')).type('c').press_enter()
 
+    # Checking that the was successfully add elements
     browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
 
-    # toggle 'b'
-    toggle_b = browser.element('li#ember11')
-    toggle_b.element('input.toggle').click()
+    # Mark the element as completed
+    browser.element('#todo-list>li#ember11').element('input.toggle').click()
 
-
-    # completed todos should be b
-    toggle_b.element('label').should(have.exact_text('b'))
-
-    # active todos should be a, c
-    toggle_b.element('label').should(have.no.exact_texts('a', 'c'))
+    # Checking which element is completed and which are active
+    browser.element('#todo-list>li.completed').element('label').should(have.exact_text('b'))
+    browser.element('#todo-list>li.completed').element('label').should(have.no.exact_texts('a', 'c'))
