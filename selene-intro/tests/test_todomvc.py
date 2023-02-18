7aecafac94d8a7aa8e0788e98ba8c7cb44a1e8c3
DimaQA <dimon.chernishov@gmail.com>
2023-02-18 13:57:45 +0200

    Changing the selector using the nth element and a new version of the checks

--- selene-intro/tests/test_todomvc.py
+++ selene-intro/tests/test_todomvc.py
@@ -1,4 +1,4 @@
-from selene import by, be, have
+from selene import by, have
 from selene.support.shared import browser
 
 
@@ -15,8 +15,8 @@ def test_complete_todo():
     browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
 
     # Mark the element as completed
-    browser.element('#todo-list>li#ember11').element('input.toggle').click()
+    browser.element('#todo-list>li:nth-of-type(2)').element('input.toggle').click()
 
     # Checking which element is completed and which are active
-    browser.element('#todo-list>li.completed').element('label').should(have.exact_text('b'))
-    browser.element('#todo-list>li.completed').element('label').should(have.no.exact_texts('a', 'c'))
+    browser.all('#todo-list>li.completed').should(have.exact_texts('b'))
+    browser.all('#todo-list>li:not(.completed)').should(have.exact_texts('a', 'c'))
