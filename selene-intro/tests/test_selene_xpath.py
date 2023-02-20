be31686526509be5991b7eacc7c113fc31ced707
DimaQA <dimon.chernishov@gmail.com>
2023-02-20 14:02:02 +0200

    Changed the locators, removed the comments and used one class from others

--- selene-intro/tests/test_selene_xpath.py
+++ selene-intro/tests/test_selene_xpath.py
@@ -6,17 +6,13 @@ def test_complete_task():
     # open TodoMVC page
     browser.open('https://todomvc.com/examples/emberjs/')
 
-    # Try to add elements in to do list
-    browser.element(by.xpath('//*[@id="new-todo"]')).type('a').press_enter()
-    browser.element(by.xpath('//header/input')).type('b').press_enter()
-    browser.element(by.xpath('//input')).type('c').press_enter()
+    browser.element('//*[@id="new-todo"]').type('a').press_enter()
+    browser.element('//*[@id="new-todo"]').type('b').press_enter()
+    browser.element('//*[@id="new-todo"]').type('c').press_enter()
 
-    # Checking that the added elements present in list
-    browser.elements('//*[@id="todo-list"]//label').should(have.exact_texts('a', 'b', 'c'))
+    browser.all('//*[@id="todo-list"]//li').should(have.exact_texts('a', 'b', 'c'))
 
-    # Do completed element b
-    browser.element('//li[2]//input[@class="toggle"]').click()
+    browser.all('//*[@id="todo-list"]//li').element_by(have.exact_text('b')).element('.toggle').click()
 
-    # Checking which element is completed and which are active
-    browser.all('//li[@class="completed ember-view"]//label').should(have.exact_texts('b'))
-    browser.all('//li[@class="completed ember-view"]//label').should(have.no.exact_texts('a', 'c'))
+    browser.all('//*[@id="todo-list"]//li[@class="completed"]').element_by(have.exact_text('b'))
+    browser.all('//*[@id="todo-list"]//li[@class="completed"]').element_by(have.no.exact_texts('a', 'c'))
