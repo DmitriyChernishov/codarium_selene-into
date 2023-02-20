8a3124f654b72a04f9bb07d507af8efbec480c39
DimaQA <dimon.chernishov@gmail.com>
2023-02-20 13:54:04 +0200

    Remove comment and use the same style for searching

--- selene-intro/tests/test_todomvc.py
+++ selene-intro/tests/test_todomvc.py
@@ -6,17 +6,13 @@ def test_complete_todo():
     # open TodoMVC page
     browser.open('https://todomvc.com/examples/emberjs/')
 
-    # Try to add elements in to do
-    browser.element(by.id('new-todo')).type('a').press_enter()
-    browser.element(by.id('new-todo')).type('b').press_enter()
-    browser.element(by.id('new-todo')).type('c').press_enter()
+    browser.element('#new-todo').type('a').press_enter()
+    browser.element('#new-todo').type('b').press_enter()
+    browser.element('#new-todo').type('c').press_enter()
 
-    # Checking that the was successfully add elements
     browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
 
-    # Mark the element as completed
-    browser.element('#todo-list>li:nth-of-type(2)').element('input.toggle').click()
+    browser.element('#todo-list>li:nth-of-type(2)').element('.toggle').click()
 
-    # Checking which element is completed and which are active
     browser.all('#todo-list>li.completed').should(have.exact_texts('b'))
     browser.all('#todo-list>li:not(.completed)').should(have.exact_texts('a', 'c'))
