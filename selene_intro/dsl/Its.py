0a31e332d561bd6e9bbd291405d3198df546aa96
DimaQA <dimon.chernishov@gmail.com>
2023-05-22 09:49:43 +0300

    next stage with build patterns

--- selene_intro/dsl/Its.py
+++ selene_intro/dsl/Its.py
@@ -1,13 +1,15 @@
 class Its:
-    @staticmethod
+    def __init__(self, xpath=""):
+        self.xpath = xpath
+
     def have_id(self, id):
         self.xpath += f"//*[@id='{id}']"
         return self
 
     def with_class(self, class_name):
         self.xpath += f"[contains(@class, '{class_name}')]"
-        return self
+        return self #f"[contains(@class, '{class_name}')]"
 
     def with_text(self, text):
         self.xpath += f"[.//text()='{text}']"
-        return self
+        return self #f"[.//text()='{text}']"
--- tests/xpath/test_todomvc_dsl.py
+++ tests/xpath/test_todomvc_dsl.py
@@ -3,8 +3,8 @@ from selene.support.shared import browser
 from selene_intro.helpers import XPathBuilder
 from selene_intro.dsl import x, its
 
-
-def test_add_and_complete_todo():
+"""
+def add_and_complete_todo():
     # Given
     browser.open('https://todomvc.com/examples/emberjs/')
 
@@ -18,7 +18,7 @@ def test_add_and_complete_todo():
             .child(XPathBuilder.tag("*").with_class("toggle").build())\
             .build()
     browser.element((f'//*[@id="todo-list"]/{result}')).click()
-
+"""
 
 def test_complete_todo_dsl():
     # Given
